10bbbbee5f4557c15aeed0880efdde6b7ac67b99
Chase May <chaseloganmay@gmail.com>
2020-09-24 11:19:24 -0600

    Update extensions

--- .config/vscode/install-exts.sh
+++ .config/vscode/install-exts.sh
@@ -1,3 +1,9 @@
+# To generate the list of extensions, run:
+#
+# ```sh
+# code --list-extensions > ~/.config/vscode/vscode-exts.txt
+# ```
+
 while read line; do
     code --install-extension $line --force
 done <~/.config/vscode/vscode-exts.txt
