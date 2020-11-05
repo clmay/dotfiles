51e9f5f4a8dc3a4d8a8db93fc5ca8030767f99c8
Chase May <chaseloganmay@gmail.com>
2020-11-05 12:08:43 -0700

    Factor out init steps by tooling

--- .config/init.sh
+++ .config/init.sh
@@ -1,14 +1,3 @@
-# Homebrew
-# --------
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-# Install Homebrew packages
-brew bundle
-
-# `asdf`
-# ------
-asdf plugin add golang
-asdf plugin add nodejs
-asdf plugin add yarn
-
-asdf install
+. ~/.config/init/brew.sh
+. ~/.config/init/asdf.sh
+. ~/.config/init/vscode.sh
