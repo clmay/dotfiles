0a7be95de673d85d00494feaedce5145f9d73a74
Chase May <chaseloganmay@gmail.com>
2020-11-05 11:14:00 -0700

    Move VS Code config files

--- .config/init/vscode.sh
+++ .config/init/vscode.sh
@@ -1,9 +1,9 @@
 # To generate the list of extensions, run:
 #
 # ```sh
-# code --list-extensions > ~/.config/vscode/vscode-exts.txt
+# code --list-extensions > ~/.config/init/vscode-exts.txt
 # ```
 
 while read line; do
     code --install-extension $line --force
-done <~/.config/vscode/vscode-exts.txt
+done <~/.config/init/vscode-exts.txt
