35dffea5faadb033a4b7d27ebdf098ba57977663
Chase May <chaseloganmay@gmail.com>
2020-11-07 12:43:52 -0700

    Add `asdf` init script and plugins list

---
+++ .config/init/asdf.sh
@@ -0,0 +1,17 @@
+# To generate the list of extensions, run:
+#
+# ```sh
+# asdf plugin list > ~/.config/init/asdf-plugins.txt
+# ```
+
+while read line; do
+  asdf plugin add $line
+done <~/.config/init/asdf-plugins.txt
+
+asdf plugin update --all
+
+# Add Node.js release keys
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+
+asdf install
+asdf reshim
