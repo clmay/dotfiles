660537af2b95b8b58989141ae8e23e9f337b1d7a
Chase May <chaseloganmay@gmail.com>
2020-11-10 15:43:16 -0700

    Reorganize, add plugin/ext files

--- .config/init.sh
+++
@@ -1,7 +0,0 @@
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-# Run idempotent setup scripts
-. ~/.config/brew.sh
-. ~/.config/asdf.sh
-. ~/.config/vscode.sh
