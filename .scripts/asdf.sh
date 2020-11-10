6b176aeaaeb9a42c1dcf97629556e449cf7329c3
Chase May <chaseloganmay@gmail.com>
2020-11-10 16:09:58 -0700

    Remove .tool-versions and update `asdf` script

--- .scripts/asdf.sh
+++ .scripts/asdf.sh
@@ -6,11 +6,11 @@
 
 while read line; do
   asdf plugin add $line
+  asdf install $line latest
+  asdf global $line latest
 done <~/.config/asdf-plugins.txt
 
 asdf plugin update --all
-
-asdf install
 asdf reshim
 
 asdf plugin list >~/.config/asdf-plugins.txt
