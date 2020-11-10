caa81cd7a5d65b85ff010fbbaa58556f498d2004
Chase May <chaseloganmay@gmail.com>
2020-11-10 15:48:18 -0700

    Reorder steps

--- .scripts/brew.sh
+++ .scripts/brew.sh
@@ -1,12 +1,12 @@
 brew update
 brew bundle
 
-# Remove non-dependencies
-brew bundle cleanup -f
-
 # Upgrade all
 brew upgrade
 brew upgrade --cask
+
+# Remove non-dependencies
+brew bundle cleanup -f
 
 # Update Brewfile
 brew bundle dump -f
