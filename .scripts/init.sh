1d40c439a53aef6c56c9fce76b6ed995495652be
Chase May <chaseloganmay@gmail.com>
2020-11-10 16:02:11 -0700

    Refactor

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -3,17 +3,4 @@ echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo
 
-# Run idempotent setup scripts
-echo "Running Homebrew script"
-echo
-. ~/.scripts/brew.sh
-echo
-
-echo "Running \`asdf\` script"
-echo
-. ~/.scripts/asdf.sh
-echo
-
-echo "Running VS Code script"
-echo
-. ~/.scripts/vscode.sh
+. ~/.scripts/sync.sh
