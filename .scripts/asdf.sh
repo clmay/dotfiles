ae73efbdfa2e22933f8bd42f646eadf16798c039
Chase May <chaseloganmay@gmail.com>
2020-11-10 20:27:34 -0700

    Update `asdf` script again

--- .scripts/asdf.sh
+++ .scripts/asdf.sh
@@ -1,16 +1,12 @@
-# To generate the list of extensions, run:
-#
-# ```sh
-# asdf plugin list > ~/.config/init/asdf-plugins.txt
-# ```
+touch .tool-versions
 
 while read line; do
   asdf plugin add $line
-  asdf install $line latest
-  asdf global $line latest
 done <~/.config/asdf-plugins.txt
 
 asdf plugin update --all
+
+asdf install
 asdf reshim
 
 asdf plugin list >~/.config/asdf-plugins.txt
