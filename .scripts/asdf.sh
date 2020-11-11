cb8cbd69fa71412623f1aed917daf06ff617f311
Chase May <chaseloganmay@gmail.com>
2020-11-11 11:05:48 -0700

    Remove comments

--- .scripts/asdf.sh
+++ .scripts/asdf.sh
@@ -1,6 +1,3 @@
-# Update asdf plugin list with the following command:
-# asdf plugin list >~/.config/asdf-plugins.txt
-
 while read line; do
   asdf plugin add $line
 done <~/.config/asdf-plugins.txt
