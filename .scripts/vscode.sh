1d40722057926ad19ab7eb3cbc8ad075a061325a
Chase May <chaseloganmay@gmail.com>
2021-03-29 12:12:02 -0700

    Update VS Code extensions script

--- .scripts/vscode.sh
+++ .scripts/vscode.sh
@@ -1,4 +1,4 @@
-code --list-extensions >~/.config/vscode/vscode-exts.txt
+# code --list-extensions >~/.config/vscode/vscode-exts.txt
 
 while read line; do
     code --install-extension $line --force
