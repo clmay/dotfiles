e2afd1c9ec2149822d7fdb047e0e867fed0f487f
Chase May <chaseloganmay@gmail.com>
2021-03-29 12:14:47 -0700

    Update scripts

--- .scripts/brew.sh
+++ .scripts/brew.sh
@@ -2,7 +2,6 @@ brew bundle
 
 # Upgrade all
 brew upgrade
-brew upgrade --cask
 
 # Update Brewfile
 brew bundle dump -f
--- .scripts/vscode.sh
+++ .scripts/vscode.sh
@@ -1,5 +1,5 @@
-# code --list-extensions >~/.config/vscode/vscode-exts.txt
-
 while read line; do
     code --install-extension $line --force
 done <~/.config/vscode/vscode-exts.txt
+
+code --list-extensions >~/.config/vscode/vscode-exts.txt
