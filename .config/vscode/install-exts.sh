0e67feb0fad0a9849e512adfa9f9d01f47ae1382
Chase May <clmay@protonmail.com>
2023-03-02 11:53:25 -0800

    Update

--- .config/vscode/install-exts.sh
+++ .config/vscode/install-exts.sh
@@ -1,5 +1,5 @@
+#!/bin/sh
+
 while read line; do
     code --install-extension $line --force
 done <~/.config/vscode/vscode-exts.txt
-
-code --list-extensions >~/.config/vscode/vscode-exts.txt
---
+++ .config/vscode/update-list.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+code --list-extensions >~/.config/vscode/vscode-exts.txt
