4377e4fa6157fc1ae05fadfcb721f6a19b52ad48
Chase May <chase.may@churchofjesuschrist.org>
2023-04-26 09:25:52 -0700

    Add some logging for readability

--- .config/asdf/sync.sh
+++ .config/asdf/sync.sh
@@ -8,14 +8,19 @@ done <~/.config/asdf/plugins.txt
 
 # install and set global version for every plugin
 for plugin in "${plugins[@]}"; do
+    echo "Working on \`$plugin\`..."
     asdf plugin add $plugin
     asdf plugin update $plugin
     asdf install $plugin latest
     asdf global $plugin latest
+    echo "Done working on \`$plugin\`."
+    echo
 done
 
 # update asdf shims
+echo "Reshimming asdf..."
 asdf reshim
 
 # output the list of plugins back to the file
+echo "Updating plugins.txt..."
 asdf plugin list >~/.config/asdf/plugins.txt
--- .config/vscode/sync.sh
+++ .config/vscode/sync.sh
@@ -2,14 +2,22 @@
 
 # read extensions from extensions.txt into an array
 extensions=()
+
+echo "Reading extensions.txt..."
+echo
+
 while read extension; do
     extensions+=($extension)
 done <~/.config/vscode/extensions.txt
 
 # for every extension in the array, install it
 for extension in "${extensions[@]}"; do
+    echo "Installing $extension..."
     code --install-extension $extension --force
+    echo "Done installing $extension."
+    echo
 done
 
 # output the list of extensions back to the file
+echo "Updating extensions.txt..."
 code --list-extensions >~/.config/vscode/extensions.txt
