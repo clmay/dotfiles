c580f69340882789ddf9f1f811d8c6b10ee402ef
Chase May <clmay@protonmail.com>
2023-12-01 23:09:39 -0800

    Update script; remove plugins

--- .config/asdf/sync.sh
+++ .config/asdf/sync.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
-# install and set global version for every plugin
-for plugin in deno direnv elixir erlang nodejs ruby; do
-    echo Working on \`$plugin\`...
+# read plugins.txt
+# plugins=()
+while read -r line; do
+    plugins+=("$line")
+done <~/.config/asdf/plugins.txt
+
+# add plugins
+echo "Adding plugins..."
+echo "================="
+echo
+for plugin in ${plugins[@]}; do
     asdf plugin add $plugin
+done
+echo
+
+# update plugins
+echo "Updating plugins..."
+echo "==================="
+echo
+for plugin in ${plugins[@]}; do
     asdf plugin update $plugin
-    asdf install $plugin latest
-    asdf global $plugin latest
-    echo Done working on \`$plugin\`.
     echo
 done
+# no echo here
+
+# install latest versions of plugins
+echo "Installing latest versions..."
+echo "============================="
+echo
+for plugin in ${plugins[@]}; do
+    asdf install $plugin latest
+done
+echo
+
+# set global versions
+echo "Setting global versions..."
+for plugin in ${plugins[@]}; do
+    asdf global $plugin latest
+done
+# no echo here
 
 # update asdf shims
-echo Reshimming asdf...
+echo "Reshimming asdf..."
 asdf reshim
 
-# output the list of plugins back to the file
-echo Updating plugins.txt...
+# update plugins.txt
+echo "Updating plugins.txt..."
 asdf plugin list >~/.config/asdf/plugins.txt
